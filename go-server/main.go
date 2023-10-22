9bd136127e8b5c983c38a9b340e65f92da66d628
Amauri Gomes <62269987+amaurineo@users.noreply.github.com>
2023-10-22 17:33:06 -0300

    To do Feito

--- go-server/main.go
+++ go-server/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type Resource struct {
@@ -18,15 +19,15 @@ type Resource struct {
 	Values      []int  `json:"values"`
 }
 
-func seedDB(conn *pgx.Conn) (pgx.Rows, error) {
-	rows, err := conn.Query(context.Background(), `CREATE TABLE IF NOT EXISTS resources (
+func seedDB(conn *pgxpool.Pool) error {
+	_, err := conn.Query(context.Background(), `CREATE TABLE IF NOT EXISTS resources (
       id SERIAL PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       description TEXT NOT NULL,
       values INT[] NOT NULL
     );`)
 
-	return rows, err
+	return err
 }
 
 // 2. Criar um canal para receber os dados do json
@@ -35,15 +36,15 @@ func seedDB(conn *pgx.Conn) (pgx.Rows, error) {
 // conn.Query(context.Background(), "insert into resources(name, description, values) values($1, $2, $3)", resource.Name, resource.Description, resource.Values)
 
 func main() {
-	conn, err := pgx.Connect(context.Background(), "postgres://postgres:postgres@localhost:5432/postgres")
+	conn, err := pgxpool.Connect(context.Background(), "postgres://postgres:postgres@localhost:5432/postgres")
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer conn.Close(context.Background())
+	defer conn.Close()
 
-	_, err = seedDB(conn)
+	err = seedDB(conn)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to seed database: %v\n", err)
@@ -52,12 +53,43 @@ func main() {
 
 	r := mux.NewRouter()
 
+	//⭐⭐
+	resourceChannel := make(chan Resource)
+
+	//⭐⭐⭐
+	go func() {
+		for resource := range resourceChannel {
+			_, err := conn.Exec(context.Background(), "INSERT INTO resources (name, description, values) VALUES ($1, $2, $3)", resource.Name, resource.Description, resource.Values)
+			if err != nil {
+				// Lidar com erros de inserção no banco de dados
+				fmt.Fprintf(os.Stderr, "Erro na inserção no banco de dados: %v\n", err)
+			}
+		}
+	}()
+
 	r.HandleFunc("/resources", func(w http.ResponseWriter, r *http.Request) {
 		// 1.
 		// converter o body para json: https://gowebexamples.com/json/
 		// inserir o json no canal para ser inserido no banco
 		// retornar código 202 Accepted
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+
+		//⭐
+		if r.Method != http.MethodPost {
+			http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var resource Resource
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&resource); err != nil {
+			http.Error(w, "Falha ao decodificar JSON", http.StatusBadRequest)
+			return
+		}
+
+		resourceChannel <- resource // Insira no canal
+
+		w.WriteHeader(http.StatusAccepted)
+		fmt.Fprintln(w, "Accepted")
 	}).Methods("POST")
 
 	fmt.Printf("Starting server at port 8080\n")
