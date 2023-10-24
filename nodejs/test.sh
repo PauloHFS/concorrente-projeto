f28a66ab25d3136117115dd11729cd1e38bd66db
Paulo Hernane <paulohernane10@gmail.com>
2023-10-24 19:56:59 -0300

    add test to nodejs

--- nodejs/test.sh
+++ nodejs/test.sh
@@ -1,2 +1,16 @@
 yarn build
-yarn start 2
+
+workers=(1 2 4 6 8 10 12)
+
+for num in "${workers[@]}"
+do
+  echo "$num - Starting Test with $num workers"
+  numWorkers=$num
+  database_path="db.sqlite3"
+  output_path="tests/node-$numWorkers-workers.csv"
+  mkdir -p tests
+
+  yarn start $numWorkers
+
+  sqlite3 -header -csv $database_path "SELECT * FROM resources;" > $output_path
+done
